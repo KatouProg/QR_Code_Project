7c661c3b4e478df95eb2ce9ccc2ac6515be1a5b3
KatouProg <katouprog@gmail.com>
2022-06-12 15:52:34 +0900

    FRONTEND - FontAwesome / Google fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <h1>Wesh la mifffffffffff</h1>
       <Header />
     </div>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className='header-container'>
-            <h1>I am the HEADER</h1>
-            <p>1REM = 10px</p>
+        
         </div>
     );
 };
