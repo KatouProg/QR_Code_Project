b78f0528d4fe57cd1f7743b450d802943f77fa6f
KatouProg <katouprog@gmail.com>
2022-08-15 16:00:29 +0900

    FRONT - Add Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Register />
     </div>
   );
 }
